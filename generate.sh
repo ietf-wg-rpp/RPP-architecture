e8fc0c42b98a4d2a2d7d9354def479dcb318b71a
Pawel Kowalik <pawel-kow@users.noreply.github.com>
2025-11-07 00:10:52 +0100

    Release draft-ietf-rpp-architecture-00 (#74)
    * Document renamed to draft-ietf-rpp-architecture-00
    * Generate draft-ietf-rpp-architecture-00

--- generate.sh
+++ generate.sh
@@ -14,7 +14,7 @@ display_help() {
 
 # Check if a parameter is provided
 if [ -z "$1" ]; then
-    filename=`echo draft-kowalik-rpp-architecture-*.adoc | sed 's/.adoc$//'`
+    filename=`echo draft-ietf-rpp-architecture-*.adoc | sed 's/.adoc$//'`
     echo "warning: using default filename: $filename" >&2
 else
     filename="$1"
